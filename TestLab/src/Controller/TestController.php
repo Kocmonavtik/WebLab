09a651edde8eb9cdcbca64d90562f10b6d916a1a
Kocmonavtik <chapligin141299@gmail.com>
2022-04-05 21:33:41 +0300

    Add Controller

---
+++ TestLab/src/Controller/TestController.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Repository\BookRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/", name="startpage")
+     *@param BookRepository $bookRep
+     *@param Request $request
+     *
+     * @return Response
+     */
+    public function index(BookRepository $bookRep,Request $request): Response
+    {
+        $books = $bookRep->findBy(
+            [],
+            ['dateRead' => 'DESC']
+        );
+        $user = '';
+        if ($this->getUser() !== null) {
+            $user = $this->getUser()->getUsername();
+        }
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'AboutController',
+            'books' => $books,
+            'user' => $user,
+        ]);
+    }
+
+
+
+
+
+
+    ///homepage/{name} name="homepage"
+    //$greet='';
+    //if($name){
+    //   $greet=sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+    //}
+    //return $this->render('about/index.html.twig', [
+    //     'controller_name' => 'AboutController',
+    // ]);
+    /*return new Response(<<<EOF
+<html>
+<body>
+    <img src="/images/under-construction.gif" />
+</body>
+</html>
+EOF
+    );*/
+}
